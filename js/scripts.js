97ad98aa0b2b83e18b4e4b59449dff6c420f3c03
SiderFace <115373215+SiderFace@users.noreply.github.com>
2022-11-16 22:00:29 -0500

    Data Entry: First batch of Pokemon for Pokedex
    Deleted previous Alert-greeting code, and entered nine (9) Pokemon to start the Pokedex database.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
-alert('Hello World') ;
+let pokemonList = [
+   
+   {name: 'Psyduck', height: '0.8', type: ['water', 'psychic']} ,
+   {name: 'Minccino', height: '0.4', type: 'normal'} ,
+   {name: 'Lapras', height: '2.5', type: ['ice', 'water']} ,
+   {name: 'Areceus', height: '3.2', type: 'normal'} ,
+   {name: 'Pichu', height: '0.3', type: 'electric'} ,
+   {name: 'Pidget', height: '0.3', type: ['flying', 'normal']} ,
+   {name: 'Hoppip', height: '0.4', type: ['grass', 'flying']} ,
+   {name: 'Arcanine', height: '1.9', type: 'fire'} ,
+   {name: 'Onix', height: '8.8', type: ['rock', 'ground']} ,
 
-let myName = 'Sean Siders' ;
-
-let favoriteFood = 'Lobster Ravioli' ;
-document.write(favoriteFood) ;
+]
