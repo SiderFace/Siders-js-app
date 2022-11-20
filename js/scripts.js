21e1c3adccc41a393e7f02418ac774df98123d0f
SiderFace <115373215+SiderFace@users.noreply.github.com>
2022-11-20 16:51:07 -0500

    for.loop to document.write the array items
    Created a for.loop to document.write all of the items of the array of Pokemon, in no particular order.  Added text to comment on Pokemon with a height of 5.0 meters or greater.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 let pokemonList = [
-   
+
    {name: 'Psyduck', height: '0.8', type: ['water', 'psychic']} ,
-   {name: 'Minccino', height: '0.4', type: 'normal'} ,
+   {name: 'Minccino', height: '0.4', type: ['normal']} ,
    {name: 'Lapras', height: '2.5', type: ['ice', 'water']} ,
-   {name: 'Areceus', height: '3.2', type: 'normal'} ,
-   {name: 'Pichu', height: '0.3', type: 'electric'} ,
+   {name: 'Areceus', height: '3.2', type: ['normal']} ,
+   {name: 'Pichu', height: '0.3', type: ['electric']} ,
+   {name: 'Onix', height: '8.8', type: ['rock', 'ground']} ,
    {name: 'Pidget', height: '0.3', type: ['flying', 'normal']} ,
    {name: 'Hoppip', height: '0.4', type: ['grass', 'flying']} ,
-   {name: 'Arcanine', height: '1.9', type: 'fire'} ,
-   {name: 'Onix', height: '8.8', type: ['rock', 'ground']} ,
-
+   {name: 'Arcanine', height: '1.9', type: ['fire']} ,  
 ]
+
+for (let i=0; i < pokemonList.length; i++) {
+   document.write(pokemonList[i].name + ' ' + '(Height: ' + pokemonList[i].height + ')') ;
+   if (pokemonList[i].height >= 5.0) {document.write (" - Wow! That's Big!")} ;
+   document.write('</br>')
+}
